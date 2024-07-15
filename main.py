1c4622c2ab57257981e34f565639e8f597cfc12e
Binaykumar12 <binaykumarkurmi78@gmail.com>
2024-07-15 23:04:45 +0545

    /

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ from bs4 import BeautifulSoup
 import random
 import time
 
-# Define a single set of headers
 headers = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36",
     "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
